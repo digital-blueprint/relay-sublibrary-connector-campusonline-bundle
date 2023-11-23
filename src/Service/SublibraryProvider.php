fe116ffea1795a6a1a45ec7884c26a18e3078938
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2023-11-23 12:32:33 +0100

    adapt to version 0.2.0 of the sublibrary bundle

--- src/Service/SublibraryProvider.php
+++ src/Service/SublibraryProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Dbp\Relay\SublibraryConnectorCampusonlineBundle\Service;
 
 use Dbp\Relay\BaseOrganizationBundle\API\OrganizationProviderInterface;
-use Dbp\Relay\BasePersonBundle\Entity\Person;
 use Dbp\Relay\CoreBundle\Exception\ApiError;
 use Dbp\Relay\CoreBundle\Rest\Options;
 use Dbp\Relay\SublibraryBundle\API\SublibraryInterface;
@@ -17,7 +16,6 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 class SublibraryProvider implements SublibraryProviderInterface
 {
     public const ORGANIZATION_CODE_ATTRIBUTE_NAME = 'code';
-    public const TUG_FUNCTIONS_ATTRIBUTE_NAME = 'tugFunctions';
 
     /** @var OrganizationProviderInterface */
     private $organizationProvider;
@@ -32,51 +30,6 @@ class SublibraryProvider implements SublibraryProviderInterface
     }
 
     public function getSublibrary(string $identifier, array $options = []): ?SublibraryInterface
-    {
-        return $this->getSublibraryInternal($identifier, $options);
-    }
-
-    /**
-     * Returns the array of sub-library IDs the given Person is a library manager of.
-     *
-     * @return string[]
-     */
-    public function getSublibraryIdsByLibraryManager(Person $person): array
-    {
-        return $this->getSublibrariesByLibraryManagerInternal($person, true);
-    }
-
-    /**
-     * Returns the array of sub-library codes the given Person is a library manager of.
-     *
-     * @return string[]
-     */
-    public function getSublibraryCodesByLibraryManager(Person $person): array
-    {
-        return $this->getSublibrariesByLibraryManagerInternal($person, false);
-    }
-
-    /*
-     * Returns whether the given Person is a library manager of the Sublibrary with the given ID.
-     *
-     * @param string $sublibraryId The Sublibrary ID
-     */
-    public function isLibraryManagerById(Person $person, string $sublibraryId): bool
-    {
-        return in_array($sublibraryId, $this->getSublibrariesByLibraryManagerInternal($person, true), true);
-    }
-
-    /*
-     * Returns whether the given Person is a library manager of the Sublibrary with the given code.
-     *
-     * @param string $sublibraryCode The Sublibrary code
-     */
-    public function isLibraryManagerByCode(Person $person, string $sublibraryCode): bool
-    {
-        return in_array($sublibraryCode, $this->getSublibrariesByLibraryManagerInternal($person, false), true);
-    }
-
-    private function getSublibraryInternal(string $identifier, array $options): ?Sublibrary
     {
         $organization = null;
         try {
@@ -97,37 +50,5 @@ class SublibraryProvider implements SublibraryProviderInterface
         $this->eventDispatcher->dispatch($postEvent);
 
         return $postEvent->getSublibrary();
-    }
-
-    /**
-     * Gets the list of Sublibrary IDs/codes the given person is a library manager of. This function is currently TU Graz specific.
-     *
-     * @param bool $getIds If true, the Sublibrary IDs are returned, the Sublibrary codes otherwise
-     *
-     * @return string[]
-     */
-    private function getSublibrariesByLibraryManagerInternal(Person $person, bool $getIds): array
-    {
-        $sublibraries = [];
-        $regex = "/^F_BIB:F:(\d+):([\d_]+)$/i";
-        $functions = $person->getLocalDataValue(self::TUG_FUNCTIONS_ATTRIBUTE_NAME);
-
-        if ($functions !== null) {
-            foreach ($functions as $function) {
-                if (preg_match($regex, $function, $matches)) {
-                    $sublibrary = $getIds ? $matches[2] : 'F'.$matches[1];
-                    if (!$getIds) {
-                        if (strlen($sublibrary) === 6) {
-                            $sublibrary = substr($sublibrary, 0, -1);
-                        }
-                    }
-                    if (!in_array($sublibrary, $sublibraries, true)) {
-                        $sublibraries[] = $sublibrary;
-                    }
-                }
-            }
-        }
-
-        return $sublibraries;
     }
 }
--- tests/Service/SublibraryProviderTest.php
+++ tests/Service/SublibraryProviderTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Dbp\Relay\SublibraryConnectorCampusonlineBundle\Tests\Service;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-use Dbp\Relay\BasePersonBundle\Entity\Person;
 use Dbp\Relay\SublibraryConnectorCampusonlineBundle\Service\SublibraryProvider;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
@@ -27,70 +26,5 @@ class SublibraryProviderTest extends ApiTestCase
         $this->assertSame($sublibraryId, $sublibrary->getIdentifier());
         $this->assertSame(TestOrganizationProvider::toOrganizationName($sublibraryId), $sublibrary->getName());
         $this->assertSame(TestOrganizationProvider::toOrganizationCode($sublibraryId), $sublibrary->getCode());
-    }
-
-    public function testGetSublibraryIdsByLibraryManager(): void
-    {
-        $functions = [
-            'ANG:D:95300:34886',
-            'F_BIB:F:1490:681',
-            'F_BIB:F:1610:14128',
-        ];
-
-        $person = new Person();
-        $person->setLocalDataValue(SublibraryProvider::TUG_FUNCTIONS_ATTRIBUTE_NAME, $functions);
-
-        $sublibraryIds = $this->sublibraryProvider->getSublibraryIdsByLibraryManager($person);
-        $this->assertCount(2, $sublibraryIds);
-        $this->assertSame('681', $sublibraryIds[0]);
-        $this->assertSame('14128', $sublibraryIds[1]);
-    }
-
-    public function testGetSublibraryCodesByLibraryManager(): void
-    {
-        $functions = [
-            'ANG:D:95300:34886',
-            'F_BIB:F:1490:681',
-            'F_BIB:F:1610:14128',
-        ];
-
-        $person = new Person();
-        $person->setLocalDataValue(SublibraryProvider::TUG_FUNCTIONS_ATTRIBUTE_NAME, $functions);
-
-        $sublibraryCodes = $this->sublibraryProvider->getSublibraryCodesByLibraryManager($person);
-        $this->assertCount(2, $sublibraryCodes);
-        $this->assertSame('F1490', $sublibraryCodes[0]);
-        $this->assertSame('F1610', $sublibraryCodes[1]);
-    }
-
-    public function testGetSublibrariesByPersonNoFunctions()
-    {
-        $person = new Person();
-        $person->setLocalDataValue(SublibraryProvider::TUG_FUNCTIONS_ATTRIBUTE_NAME, []);
-        $result = $this->sublibraryProvider->getSublibraryIdsByLibraryManager($person);
-        $this->assertSame([], $result);
-        $result = $this->sublibraryProvider->getSublibraryCodesByLibraryManager($person);
-        $this->assertSame([], $result);
-    }
-
-    public function testGetSublibrariesByPersonUnknownFunction()
-    {
-        $person = new Person();
-        $person->setLocalDataValue(SublibraryProvider::TUG_FUNCTIONS_ATTRIBUTE_NAME, ['nope']);
-        $result = $this->sublibraryProvider->getSublibraryIdsByLibraryManager($person);
-        $this->assertSame([], $result);
-        $result = $this->sublibraryProvider->getSublibraryCodesByLibraryManager($person);
-        $this->assertSame([], $result);
-    }
-
-    public function testGetSublibrariesByPersonOtherFunction()
-    {
-        $person = new Person();
-        $person->setLocalDataValue(SublibraryProvider::TUG_FUNCTIONS_ATTRIBUTE_NAME, ['ANG:D:4370:2322']);
-
-        $result = $this->sublibraryProvider->getSublibraryIdsByLibraryManager($person);
-        $this->assertSame([], $result);
-        $result = $this->sublibraryProvider->getSublibraryCodesByLibraryManager($person);
-        $this->assertSame([], $result);
     }
 }
